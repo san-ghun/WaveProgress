ed01db71395be01f610ef08bf6b6c9c2d18ca548
Sanghun <po4865@gmail.com>
2022-09-22 22:03:28 +0200

    feat: Add progress timer with percentage label

--- WaveProgress/ViewController.swift
+++ WaveProgress/ViewController.swift
@@ -12,6 +12,9 @@ let screenWidth = UIScreen.main.bounds.size.width
 class ViewController: UIViewController {
     
     let waterWaveView = WaterWaveView()
+    
+    let dr: TimeInterval = 10.0
+    var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +33,24 @@ class ViewController: UIViewController {
         ])
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
+//            let dr = CGFloat(1.0 / (self.dr / 0.01))
+//            
+//            self.waterWaveView.progress += dr
+//            self.waterWaveView.setupProgress(self.waterWaveView.progress)
+//            
+//            if self.waterWaveView.progress >= 0.95 {
+//                self.timer?.invalidate()
+//                self.timer = nil
+//                
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                    self.waterWaveView.percentAnim()
+//                }
+//            }
+//        })
+    }
 }
 
--- WaveProgress/WaterWaveView.swift
+++ WaveProgress/WaterWaveView.swift
@@ -13,6 +13,8 @@ class WaterWaveView: UIView {
     private let firstLayer = CAShapeLayer()
     private let secondLayer = CAShapeLayer()
     
+    private let percentLable = UILabel()
+    
     private var firstColor: UIColor = .clear
     private var secondColor: UIColor = .clear
     
@@ -51,20 +53,27 @@ class WaterWaveView: UIView {
         backgroundColor = .clear
         translatesAutoresizingMaskIntoConstraints = false
         
+        layer.cornerRadius = width / 2
         layer.masksToBounds = true
         layer.borderWidth = 1.0
         layer.borderColor = UIColor.lightGray.cgColor
         
-        waveHeight = 8.0
+        waveHeight = 15.0
         
-        firstColor = .cyan
-        secondColor = .cyan.withAlphaComponent(0.4)
+//        firstColor = .cyan
+//        secondColor = .cyan.withAlphaComponent(0.4)
+//        firstColor = UIColor(red: 0.16, green: 0.30, blue: 0.60, alpha: 1.00)
+//        secondColor = UIColor(red: 0.08, green: 0.42, blue: 0.81, alpha: 1.00)
+        firstColor = UIColor(red: 0.08, green: 0.60, blue: 0.93, alpha: 1.00)
+        secondColor = UIColor(red: 0.27, green: 0.74, blue: 1.00, alpha: 1.00)
         
         createFirstLayer()
         
         if !showSingleWave {
             createSecondLayer()
         }
+        
+        createPercentLabel()
     }
     
     private func createFirstLayer() {
@@ -81,8 +90,34 @@ class WaterWaveView: UIView {
         layer.addSublayer(secondLayer)
     }
     
+    private func createPercentLabel() {
+        percentLable.font = UIFont.boldSystemFont(ofSize: 35.0)
+        percentLable.textAlignment = .center
+        percentLable.text = ""
+        percentLable.textColor = .darkGray
+        
+        addSubview(percentLable)
+        
+        percentLable.translatesAutoresizingMaskIntoConstraints = false
+        percentLable.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        percentLable.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+    
+    func percentAnim() {
+        let anim = CABasicAnimation(keyPath: "opacity")
+        anim.duration = 1.5
+        anim.fromValue = 0.0
+        anim.toValue = 1.0
+        anim.repeatCount = .infinity
+        anim.isRemovedOnCompletion = false
+        
+        percentLable.layer.add(anim, forKey: nil)
+    }
+    
     func setupProgress(_ pr: CGFloat) {
         progress = pr
+        
+        percentLable.text = String(format: "%ld%%", NSNumber(value: Float(pr * 100)).intValue)
         
         let top: CGFloat = 1 * bounds.size.height / 2
 //        let top: CGFloat = pr * bounds.size.height
@@ -126,7 +161,7 @@ class WaterWaveView: UIView {
         bezier.close()
         
         let anim = CABasicAnimation(keyPath: "transform.translation.x")
-        anim.duration = 2.0
+        anim.duration = 3.0
         anim.fromValue = -w * 0.5
         anim.toValue = -w - w * 0.5
         anim.repeatCount = .infinity
@@ -139,13 +174,13 @@ class WaterWaveView: UIView {
         if !showSingleWave {
             let bezier = UIBezierPath()
             
-            let startOffsetY = waveHeight * CGFloat(sinf(Float(offset * twoPi * w)))
+            let startOffsetY = (waveHeight - 15) * CGFloat(sinf(Float(offset * twoPi * w)))
             var originOffsetY: CGFloat = 0.0
             
             bezier.move(to: CGPoint(x: 0.0, y: startOffsetY))
             
             for i in stride(from: 0.0, to: w * 1000, by: 1) {
-                originOffsetY = waveHeight * CGFloat(cosf(Float(twoPi / w * i + offset * twoPi / w)))
+                originOffsetY = (waveHeight - 0) * CGFloat(cosf(Float(twoPi / w * i + offset * twoPi / w)))
                 bezier.addLine(to: CGPoint(x: i, y: originOffsetY))
             }
             
@@ -156,7 +191,7 @@ class WaterWaveView: UIView {
             bezier.close()
             
             let anim2 = CABasicAnimation(keyPath: "transform.translation.x")
-            anim2.duration = 3.0
+            anim2.duration = 1.8
             anim2.fromValue = -w * 0.5
             anim2.toValue = -w - w * 0.5
             anim2.repeatCount = .infinity
